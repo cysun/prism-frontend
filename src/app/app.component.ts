9679aa1fffb2c3ddcfa141d706ba0b531723bd70
leannedavid <leanne.skit@gmail.com>
2017-12-16 17:17:11 -0800

    Create non-functional Actions menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,20 @@ import {MenuItem} from 'primeng/components/common/api';
 
 export class AppComponent {
   items: MenuItem[];
+  actionMenu: MenuItem[];
 
   ngOnInit() {
     this.items = [ { label: 'PRISM', icon: 'fa-2x fa-diamond ' }];
+
+    this.actionMenu = [{
+            label: 'Actions',
+            items: [
+                { label: 'Dashboard', icon: 'fa-home' },
+                { label: 'Calendar', icon: 'fa-calendar'},
+                { label: 'Minutes', icon: 'fa-clock-o'},
+                { label: 'Resources', icon: 'fa-folder-open-o'},
+                { label: 'Committee', icon: 'fa-users' }
+            ]
+        }];
   }
 }
