a9317262fbb5f2572708d95aa9a971d17ff97cb7
leannedavid <leanne.skit@gmail.com>
2018-01-08 13:34:01 -0800

    Create initial service for login

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+
+import { User } from '../models/user.model';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.post<User>('/api/login', { email, password })
+  }
 
 }
