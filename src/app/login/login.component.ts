300ba54724428393c6e8361ec9feabd1e4b88e75
leannedavid <leanne.skit@gmail.com>
2018-01-09 11:37:04 -0800

    Allow user to navigate to dashboard with correct credentials

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'prism-login',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+  wrongInfo: Boolean = false;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe( data => {
+      localStorage.setItem('jwt_token', data.token);
+      console.log('the token is: ' + data.token);
+
+      this.wrongInfo = false;
+      this.router.navigate(['dashboard']);
+
+    }, err => {
+      this.password = '';
+      this.wrongInfo = true;
+    })
   }
 
 }
