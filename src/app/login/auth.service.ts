dcce6a1c3581f6e65229d728873d9a3bf360f639
leannedavid <leanne.skit@gmail.com>
2018-01-09 10:31:35 -0800

    Fix login function and return token string

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -4,14 +4,28 @@ import { Observable } from 'rxjs/Rx';
 
 import { User } from '../models/user.model';
 
+interface UserResponse {
+  token: string;
+}
+
 @Injectable()
 export class AuthService {
+  public token: string;
   private TOKEN = 'jwt_token';
+  private HEADERS = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
 
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string): Observable<User> {
-    return this.http.post<User>('/api/login', { email, password })
+  login(username: string, password: string): Observable<UserResponse> {
+    const credentials = JSON.stringify({'username': username, 'password': password });
+    console.log('my credentials are: ' + credentials);
+
+    return this.http.post<UserResponse>('/api/login', credentials, this.HEADERS);
+
+    // return this.http.post('/api/login', credentials, this.HEADERS).subscribe(
+    //   // res => { console.log('Token: ' + JSON.stringify(res)); },
+    //   err => { console.log('Error has occurred.'); }
+    // )
   }
 
   isAuthenticated(): boolean {
