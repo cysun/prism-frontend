01e394d6f715d3eb41771fb4b489aa827570cc6e
leannedavid <leanne.skit@gmail.com>
2018-01-10 10:24:08 -0800

    Update user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,12 +1,27 @@
 export interface User {
   username: string;
   email: string;
+
   name: {
     first: string;
     last: string;
   };
+
   internal: boolean;
   root: boolean;
+  disabled: boolean;
   samlType: string;
   passwordHash: string;
+  
+  config: {
+    type: {
+      email: {
+        type: {
+          documentFinalized: boolean;
+          newComment: boolean;
+          meetingChange: boolean;
+        }
+      }
+    }
+  };
 }
