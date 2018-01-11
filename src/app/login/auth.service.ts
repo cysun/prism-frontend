ee8faed46a87bd14413f3da9ce8103ae45330039
leannedavid <leanne.skit@gmail.com>
2018-01-11 15:13:25 -0800

    Clear and add more console debugging for jwt

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -19,8 +19,10 @@ export class AuthService {
   }
 
   isAuthenticated(): boolean {
-    if (localStorage.getItem(this.TOKEN)) {
+    if (localStorage.getItem(this.TOKEN) && localStorage.getItem('currentUser')) {
       console.log('Is authenticated and here is the token: ' + localStorage.getItem(this.TOKEN));
+      console.log('Is authenticated and here is the user info: ' + localStorage.getItem('currentUser'));
+
       return true;
     }
     console.log('Is not authenticated');
