cef1ca7feb88e1b9a17a58d62b7de7178dc9bc25
Ben Solis <bsolis9@calstatela.edu>
2018-01-14 16:48:14 -0800

    Merge with master
    Merge the branch to update and sync files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {HttpClientModule, HttpClient, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {CollegesComponent} from './colleges/colleges.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {SettingsComponent} from './settings/settings.component';
-import {CalendarComponent} from './calendar/calendar.component';
-import {MinutesComponent} from './minutes/minutes.component';
-import {ResourcesComponent} from './resources/resources.component';
-import {CommitteeComponent} from './committee/committee.component';
-import {GroupManagerComponent} from './group-manager/group-manager.component';
 
 import {AccordionModule} from 'primeng/primeng';
 import {AutoCompleteModule} from 'primeng/primeng';
@@ -33,26 +23,6 @@ import {SplitButtonModule} from 'primeng/primeng';
 import {TabViewModule} from 'primeng/primeng';
 import {ToolbarModule} from 'primeng/primeng';
 
-=======
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { AccordionModule } from 'primeng/primeng';
-import { ButtonModule } from 'primeng/components/button/button';
-import { FieldsetModule } from 'primeng/primeng';
-import { InputTextModule } from 'primeng/primeng';
-import { MenubarModule, MenuModule, MenuItem } from 'primeng/primeng';
-import { MessagesModule, MessageModule } from 'primeng/primeng';
-import { SplitButtonModule } from 'primeng/primeng';
-import { TabViewModule } from 'primeng/primeng';
-import { ToolbarModule } from 'primeng/primeng';
-
 import { CollegesComponent } from './colleges/colleges.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -61,19 +31,17 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { MinutesComponent } from './minutes/minutes.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { CommitteeComponent } from './committee/committee.component';
->>>>>>> master
+import {GroupManagerComponent} from './group-manager/group-manager.component';
 import { LoginComponent } from './login/login.component';
 import { PublicComponent } from './layout/public/public.component';
 import { PrivateComponent } from './layout/private/private.component';
 
-<<<<<<< HEAD
 import {GroupManagerService} from './group-manager/group-manager.service';
 import {CollegesService} from './colleges/colleges.service';
-=======
+
 import { AuthInterceptor } from './login/auth.interceptor';
 import { AuthGuard } from './login/auth.guard';
 import { AuthService } from './login/auth.service';
->>>>>>> master
 
 @NgModule({
   declarations: [
@@ -94,10 +62,7 @@ import { AuthService } from './login/auth.service';
   imports: [
     BrowserModule,
     FormsModule,
-<<<<<<< HEAD
     HttpModule,
-=======
->>>>>>> master
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
