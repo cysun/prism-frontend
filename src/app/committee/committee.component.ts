c2847b7ee6b4c6de7c2ef40d3ea89173cfca530e
leannedavid <leanne.skit@gmail.com>
2018-01-30 19:54:55 -0800

    Replace committee members table

--- src/app/committee/committee.component.ts
+++ src/app/committee/committee.component.ts
@@ -11,17 +11,30 @@ import { GroupManagerService } from '../group-manager/group-manager.service';
   styleUrls: ['./committee.component.css']
 })
 export class CommitteeComponent implements OnInit {
-  prs: Group = new Group();
+  prs: any[] = [];
   currentUser: User = new User();
 
   constructor(private groupManagerService: GroupManagerService) { }
 
   ngOnInit() {
-
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'))
+
     this.groupManagerService.getPrs().subscribe( data => {
-      this.prs = data;
+      const members = data.members;
+      this.prs = members.sort(this.compareUsernames);
     })
   }
 
+  /* Function to sort group by username in alphabetical order */
+  compareUsernames(user1, user2) {
+    const username1 = user1.username.toLowerCase();
+    const username2 = user2.username.toLowerCase();
+
+    if (username1 > username2) {
+      return 1;
+    } else if (username1 < username2) {
+      return -1;
+    }
+    return 0;
+  }
 }
