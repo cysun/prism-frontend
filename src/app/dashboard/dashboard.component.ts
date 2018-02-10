2f5c1a6ae53a2722d32af5fbab88a6616d0819df
leannedavid <leanne.skit@gmail.com>
2018-02-10 14:39:47 -0800

    Display list of action logs in Recent Actions tab

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActionLogger } from '../models/action-logger.model';
+import { DashboardService } from './dashboard.service';
+
 @Component({
   selector: 'prism-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+export class DashboardComponent implements OnInit {
+  logHistory: ActionLogger = new ActionLogger();
+
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
+    this.dashboardService.getRootActionLogs().subscribe( data => {
+      this.logHistory = data;
+      console.log(data);
+    })
   }
 
 }
