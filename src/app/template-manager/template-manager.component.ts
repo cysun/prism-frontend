49b4b5887451347cc38038e14a75e9cc6f14d0bf
leannedavid <leanne.skit@gmail.com>
2018-02-25 13:27:18 -0800

    Complete method to fully create a new template

--- src/app/template-manager/template-manager.component.ts
+++ src/app/template-manager/template-manager.component.ts
@@ -21,46 +21,106 @@ export class TemplateManagerComponent implements OnInit {
   file: File;
   fileName: string;
 
-  currentTemplateId: string;
   currentTemplate: Document = new Document();
 
   constructor(private modalService: NgbModal,
-              private documentService: DocumentService,
-              private templateManagerService: TemplateManagerService,
-              private globals: Globals) { }
+    private documentService: DocumentService,
+    private templateManagerService: TemplateManagerService,
+    private globals: Globals) { }
 
-  ngOnInit() {
-    this.templateManagerService.listAllTemplates().subscribe( data => {
-      this.templates = data;
-    })
-  }
+    ngOnInit() {
+      this.listTemplates();
+    }
 
-  /* Open a basic modal passing the data of the specific template */
-  openModal(content, templateId?: string) {
-    this.modal = this.modalService.open(content, this.globals.options);
-    // this.currentTemplateId = templateId;
-    this.currentTemplate = this.templates.find( temp => temp._id === templateId);
-    console.log('printing currentTemp: ' + JSON.stringify(this.currentTemplate))
-  }
+    /* Open a basic modal passing the data of the specific template */
+    openModal(content, templateId?: string) {
+      if (templateId) {
+        this.currentTemplate = this.templates.find( temp => temp._id === templateId);
+        console.log('printing currentTemp: ' + JSON.stringify(this.currentTemplate))
+      }
+      this.modal = this.modalService.open(content, this.globals.options);
+    }
 
-  /* Close a modal */
-  closeModal() {
-    this.alert = '';
-    this.currentTemplate = new Document();
-    this.message = '';
-    this.file = null;
-    this.fileName = '';
-    this.modal.close();
-  }
-
-  /* Delete template from the list */
-  deleteTemplate() {
-    this.templateManagerService.deleteTemplate(this.currentTemplate._id).subscribe( () => {
-      console.log('Template deleted')
-      const findTemplateIndex = this.templates.findIndex( temp => temp._id === this.currentTemplate._id);
-      this.templates.splice(findTemplateIndex, 1);
+    /* Close a modal */
+    closeModal() {
+      this.alert = '';
+      this.currentTemplate = new Document();
+      this.message = '';
+      this.file = null;
+      this.fileName = '';
       this.modal.close();
-    })
-  }
+    }
 
-}
+    /* Get the total amount of revisions in the document */
+    getNumOfTemplates(templateIndex: number) {
+      return Object.keys(this.templates[templateIndex].revisions).length;
+    }
+
+    /* Set file variable to the file the user has chosen */
+    onFileChange(event) {
+      if (event.target.files.length > 0) {
+        this.file = event.target.files[0];
+        this.fileName = event.target.files[0].name;
+      }
+    }
+
+    /* Post a template */
+    postTemplate() {
+      if (this.file) {
+        this.templateManagerService.createTemplate(this.currentTemplate.title,
+          this.currentTemplate.completionEstimate).subscribe( data => {
+            this.templates.push(data);
+
+            this.postRevision(data._id).then( () => {
+              const temp = this.templates.find(item => item._id === data._id);
+              this.uploadFile(data._id, temp.revisions.length);
+              this.closeModal();
+            })
+          }, (err) => {
+            console.log(err)
+          });
+        } else {
+          this.alert = { message: 'Please attach a file.' };
+        }
+      }
+
+      /* Upload revision message and the file being sent */
+      postRevision(templateId: string) {
+        return new Promise((resolve, reject) => {
+          this.documentService.postRevision(templateId, this.message).subscribe( () => {
+            resolve();
+          }, (err) => {
+            console.log(err)
+            reject();
+          });
+        });
+      }
+
+      /* Upload the file along with the revision message */
+      uploadFile(templateId: string, templateIndex: number) {
+        this.documentService.uploadFile(templateId, templateIndex, this.file).subscribe( data => {
+          this.listTemplates();
+          console.log(data)
+        }, (err) => {
+          console.log(err);
+        });
+      }
+
+      /* Get templates */
+      listTemplates() {
+        this.templateManagerService.listAllTemplates().subscribe( data => {
+          this.templates = data;
+        })
+      }
+
+      /* Delete template from the list */
+      deleteTemplate() {
+        this.templateManagerService.deleteTemplate(this.currentTemplate._id).subscribe( () => {
+          console.log('Template deleted')
+          const findTemplateIndex = this.templates.findIndex( temp => temp._id === this.currentTemplate._id);
+          this.templates.splice(findTemplateIndex, 1);
+          this.modal.close();
+        })
+      }
+
+    }
