a57d8b658dd081d82ff03d64790758ae6e8ba929
leannedavid <leanne.skit@gmail.com>
2018-03-05 19:00:58 -0800

    Update to use object models and fix document validation error

--- src/app/models/document.model.ts
+++ src/app/models/document.model.ts
@@ -1,10 +1,13 @@
+import { Comment } from './comment.model';
+import { Revision } from './revision.model';
+
 export class Document {
   _id: string;
   title: string;
 
   groups: any[];
-  revisions: any[];
-  comments: any[];
+  revisions: Revision[];
+  comments: Comment[];
   subscribers: any[];
 
   template: boolean;
