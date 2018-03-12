47e5dafd941d8c3851f6d79af9cb22593fee4c23
Ben Solis <bsolis9@calstatela.edu>
2018-03-12 12:10:22 -0700

    Create resource service

---
+++ src/app/resources/resource.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+
+import { Resource } from '../models/resource.model';
+
+@Injectable()
+export class ResourceService {
+
+  constructor(private http: HttpClient) { }
+
+  getResources(): Observable<Resource[]> {
+    return this.http.get<Resource[]>('/api/resources');
+  }
+
+  getResource(id): Observable<Resource> {
+    return this.http.get<Resource>('/api/resource/' + id);
+  }
+
+  addResource(resource: Resource): Observable<Resource> {
+    const body = JSON.stringify({'title': resource.title, 'message': resource.message, 'uploader': resource.uploader, 'groups': resource.groups});
+    const header = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+    return this.http.post<Resource>('/api/resources', body, header);
+  }
+
+  deleteResource(id): Observable<Resource> {
+    const header = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+    return this.http.delete<Resource>('/api/resource/' + id, header);
+  }
+
+}
