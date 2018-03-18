ac073b81699269b290f574ebc2b17861d3c3654c
amclees <andrewmclees5@gmail.com>
2018-03-18 12:35:11 -0700

    Finish initial Review service

--- src/app/review/review.service.ts
+++ src/app/review/review.service.ts
@@ -10,7 +10,48 @@ export class ReviewService {
 
   constructor(private http: HttpClient) { }
 
-  getReview(id): Observable<Review> {
-    return this.http.get<Review>('/api/review/' + id);
+  getReview(id: string): Observable<Review> {
+    return this.http.get<Review>(`/api/review/${id}`);
+  }
+
+  patchReview(id: string, changes): Observable<Review> {
+    return this.http.patch<Review>(`/api/review/${id}`, changes);
+  }
+
+  deleteReview(id: string) {
+    return this.http.delete<Review>(`/api/review/${id}`);
+  }
+
+  restoreReview(id: string) {
+    return this.http.post<Review>(`/api/review/${id}/restore`, '');
+  }
+
+  createReview(programId: string): Observable<Review> {
+    return this.http.post<Review>('/api/review', {'programId': programId});
+  }
+
+  finalizeNode(reviewId: string, nodeId: string) {
+    return this.http.post(`/api/document/${reviewId}/node/${nodeId}`, '');
+  }
+
+  setNodeFinishDate(reviewId: string, nodeId: string, finishDate: string) {
+    return this.http.patch(`/api/document/${reviewId}/node/${nodeId}`, {
+      'finishDate': finishDate
+    });
+  }
+
+  createNode(id: string, title: string, groups: string[], completionEstimate: number) {
+    return this.http.post(`/api/document/${id}/node`, {
+      'title': title,
+      'groups': groups,
+      'completionEstimate': completionEstimate
+    }, {
+      headers: new HttpHeaders({'Content-Type': 'application/json'}),
+      responseType: 'text'
+    });
+  }
+
+  getReviews(): Observable<Review[]> {
+    return this.http.get<Review[]>('/api/reviews');
   }
 }
